4ea3f5f8db37bb3f8e56c20abc47eb2b351700ab
Roman <romankret@gmx.de>
2022-09-08 16:08:36 +0200

    Project setup

---
+++ face-finder-server/src/main/java/com/facefinderserver/FaceFinderServerApplication.java
@@ -0,0 +1,12 @@
+package com.facefinderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceFinderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceFinderServerApplication.class, args);
+	}
+}
