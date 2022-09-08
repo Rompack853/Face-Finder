20deaef05e0dcb06fe5aeb612c968f569ec906cc
Roman <romankret@gmx.de>
2022-09-08 16:13:29 +0200

    Project setup

---
+++ face-finder-server/src/test/java/com/facefinderserver/FaceFinderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facefinderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceFinderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
